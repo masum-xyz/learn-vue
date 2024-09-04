98d32c5ad5751e39f8c3eaac192ca01762b79641
masum.xyz <masumichsan92@gmail.com>
2024-09-04 13:31:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+  import { ref } from "vue"
+
+  const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <h1>Counter</h1>
+    <span id="value">{{ count }}</span>
+
+    <div class="">
+      <button @click="count--" id="decrease">-</button>
+      <button @click="count++" id="increase">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+  background-color: #414141;
+  color: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+#value {
+  font-size: 50px;
+}
+
+button {
+  font-size: 16px;
+  margin: 0 5px;
+  background-color: #555555;
+  color: #fff;
+  border: none;
+  padding: 5px 20px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: #777777;
+}
+
+button:active {
+  animation: pulse 0.4s linear;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
